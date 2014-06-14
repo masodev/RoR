a9db906950646a2e911ff384f4339819bad32787
Mario Solomou <webmaster@masodev.com>
2014-06-14 15:48:02 +0300

    TDD - Model (MVC)
    Generated a Model created some data using TDD with the help of RSpec and
    capybara, completed for two of the three applications.

--- rails_studio/flix/app/controllers/movies_controller.rb
+++ rails_studio/flix/app/controllers/movies_controller.rb
@@ -1,10 +1,9 @@
 class MoviesController < ApplicationController
-
   def index
-     @movies = Movie.all
+    @movies = Movie.all
   end
 
   def show
-  	@movie = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
   end
 end
--- rails_studio/flix/app/models/movie.rb
+++ rails_studio/flix/app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
- def flop?
+  def flop?
     total_gross.blank? || total_gross < 50000000
   end
 end
