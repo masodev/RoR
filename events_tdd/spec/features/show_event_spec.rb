c47bfbaa8fd4601d079cefe88b5546f116960eeb
Mario Solomou <webmaster@masodev.com>
2014-06-16 23:33:24 +0300

    TDD - Link Pages
    Completed for events.

--- events_tdd/spec/features/show_event_spec.rb
+++ events_tdd/spec/features/show_event_spec.rb
@@ -1,23 +1,17 @@
 require 'rails_helper'
 
-
 describe "Viewing an individual event" do
   
   it "shows the event's details" do
-    event = Event.create(name: "BugSmash",
-                          location: "Denver",
-                          price: 10.00,
-                          description: 'A fun evening of bug smashing!',
-                          starts_at: 10.days.from_now)
-
-    
+    event = Event.create(event_attributes(price: 20.00))
+                          
     visit event_url(event)
-
+    
     expect(page).to have_text(event.name)
     expect(page).to have_text(event.location)
-    expect(page).to have_text(event.description[0..10])
+    expect(page).to have_text("$20.00")
+    expect(page).to have_text(event.description)
     expect(page).to have_text(event.starts_at)
-    expect(page).to have_text("$10.00")
   end
   
 end
